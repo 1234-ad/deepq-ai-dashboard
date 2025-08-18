c5e2361655fc51d00506401cc3a03611c639922b
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-08-18 10:29:15 +0530

    Add admin configuration for dashboard models

---
+++ dashboard/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import WorldBankData
+
+
+@admin.register(WorldBankData)
+class WorldBankDataAdmin(admin.ModelAdmin):
+    list_display = ['country_name', 'indicator_name', 'year', 'value']
+    list_filter = ['country_name', 'indicator_name', 'year']
+    search_fields = ['country_name', 'indicator_name']
+    ordering = ['-year', 'country_name']
